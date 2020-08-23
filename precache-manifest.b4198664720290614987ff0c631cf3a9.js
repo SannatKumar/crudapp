dd40456450b663aab07fb621d852e77f2b3c4973
Raj Kumar <interplaytheheads@gmail.com>
2020-08-23 18:38:55 +0200

    Updates

---
+++ precache-manifest.b4198664720290614987ff0c631cf3a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d7573f75bfb85893a59fff4e028be3",
+    "url": "/crudapp/index.html"
+  },
+  {
+    "revision": "259ee272b7c70f19c8e2",
+    "url": "/crudapp/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "6858cd8bcbef8c55d588",
+    "url": "/crudapp/static/js/2.80e94dbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crudapp/static/js/2.80e94dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259ee272b7c70f19c8e2",
+    "url": "/crudapp/static/js/main.fb78b9ee.chunk.js"
+  },
+  {
+    "revision": "8d1326b45d6641caabf3",
+    "url": "/crudapp/static/js/runtime-main.23c4c990.js"
+  }
+]);
